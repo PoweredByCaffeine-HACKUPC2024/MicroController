eae1b536a9552f9162c8aaed6d6f2174fdb830ad
Mikigb77 <miquelgarcia.alu@gmail.com>
2024-05-04 05:28:23 +0200

    connection to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,52 @@
 #include <Arduino.h>
+#include <WiFi.h> // Include the WiFi library
+
+#define SSID "HACKUPC2024B"   // Your WiFi network SSID
+#define password "Biene2024!" // Your WiFi network password
+#define WIFI_TIMEOUT_MS 20000
 
 // put function declarations here:
-int myFunction(int, int);
+void connectToWiFi();
 
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  delay(1000);
+  connectToWiFi();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  Serial.println("hello world");
   while (1)
-    ;
+  {
+    sleep(5 * 1000);
+    // TODO update the sensor data
+    // TODO make the api call
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y)
+void connectToWiFi()
 {
-  return x + y;
+  WiFi.mode(WIFI_STA); // Optional
+  Serial.println("\nConnecting to WiFi " + String(SSID) + "...");
+  WiFi.begin(SSID, password);
+
+  unsigned long startAttemptTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("\nFailed to connect to WiFi.");
+  }
+  else
+  {
+    Serial.print("\nConnected to WiFi!\nIP address: ");
+    Serial.println(WiFi.localIP());
+  }
 }
